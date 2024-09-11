d8fadfea15a25f7f242f03f4145eb985560d7dcd
marsuk <marsukcs@gmail.com>
2024-09-11 22:20:29 +0530

    variables

---
+++ javascript-class/varri.js
@@ -0,0 +1,26 @@
+// a variable is a container for storing the data 
+// a variable behaves as if it was the value that it contains
+// types of variable => [ var,let,const]
+
+ let age = 20;
+ age = 30;
+ console.log(age);
+
+
+ const pi = 3.14;
+ console.log(pi);
+
+ var name = "marsuk";
+ console.log(name);
+
+
+// practice problems:
+/* amount = 1000;
+   output :
+          your bill amount is Rs 1000 ...
+*/
+ 
+let amount = 1000;
+console.log("your bill amount is Rs "+ amount);
+
+    
